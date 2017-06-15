aa3eb6e3b49932638fef53a85796c60c216d3b64
pjosh <jorgegar@gmail.com>
2017-06-15 09:45:05 +0200

    Routes & components

---
+++ src/components/Blog.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Header from './Header';
+
+class Blog extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        Hi!! This is the blog
+      </div>
+    )
+  };
+}
+
+export default Blog;
--- src/components/Root.js
+++ src/components/Root.js
@@ -4,11 +4,15 @@ import {
   Route
 } from 'react-router-dom'
 import Home from './Home';
+import Blog from './Blog';
+import About from './About';
 
 const Root = ({ store }) => (
   <Router>
     <div>
       <Route exact path="/" component={Home}/>
+      <Route exact path="/blog" component={Blog}/>
+      <Route exact path="/about" component={About}/>
     </div>
   </Router>
 );
