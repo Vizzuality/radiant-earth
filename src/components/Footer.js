51b9096d51075cc69d0e49886678ebc11e86d968
Hector Vizzuality <hectorvizzuality@MacBook-Air-de-Hector.local>
2017-06-15 17:11:48 +0200

    include icon arrow

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,6 +20,7 @@ class Footer extends Component {
           <div className="c-footer__newsletter-container small-6 columns align-self-middle">
             <span className="text -ff2-xs -white -uppercase -center">Subscribe to our newsletter to get latest news and updates</span>
             <form className="c-footer__newsletter-form">
+              <svg className="icon icon-logo"><use xlinkHref="#icon-arrow"></use></svg>
               <input className="text -ff2-s -uppercase" type="email" value="youremail@domain.com" />
             </form>
           </div>
