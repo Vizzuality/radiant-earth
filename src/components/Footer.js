8e9b97d639950cb236469f3ddb04e11298bd7af2
Hector Vizzuality <hectorvizzuality@MacBook-Air-de-Hector.local>
2017-06-15 16:54:13 +0200

    include svg code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,4 @@
 import React, { Component } from 'react';
-import facebook from '../images/footer/facebook.svg';
-import instagram from '../images/footer/instagram.svg';
-import linkedin from '../images/footer/linkedin.svg';
-import medium from '../images/footer/medium.svg';
-import twitter from '../images/footer/medium.svg';
-import BillMelinda from '../images/footer/BillMelinda.svg';
-import Omidyar from '../images/footer/Omidyar.svg';
-import Vizzuality from '../images/footer/Vizzuality.svg';
 
 class Footer extends Component {
   render() {
@@ -17,9 +9,9 @@ class Footer extends Component {
             <span className="text -ff2-xs -white -uppercase -center">We are working with funders and partners to accelerate open earth imagery technologies</span>
             <div className="c-footer_logos-container">
               <div className="row align-justify">
-                <img className="c-footer__logos-item" src={Omidyar} />
-                <img className="c-footer__logos-item" src={BillMelinda} />
-                <img className="c-footer__logos-item" src={Vizzuality} />
+                <svg className="icon icon-logo c-footer__logos-item"><use xlinkHref="#icon-Omidyar"></use></svg>
+                <svg className="icon icon-logo c-footer__logos-item"><use xlinkHref="#icon-BillnMelinda"></use></svg>
+                <svg className="icon icon-logo c-footer__logos-item"><use xlinkHref="#icon-Vizzuality"></use></svg>
               </div>
             </div>
           </div>
@@ -35,11 +27,11 @@ class Footer extends Component {
         <div className="row">
           <div className="c-footer__social-networks-container small-4 columns align-self-middle">
             <div className="row align-justify">
-              <img className="c-footer__logos-item" src={twitter} />
-              <img className="c-footer__logos-item" src={linkedin} />
-              <img className="c-footer__logos-item" src={facebook} />
-              <img className="c-footer__logos-item" src={medium} />
-              <img className="c-footer__logos-item" src={instagram} />
+              <svg className="icon icon-logo c-footer__logos-item"><use xlinkHref="#icon-twitter"></use></svg>
+              <svg className="icon icon-logo c-footer__logos-item"><use xlinkHref="#icon-linkedin"></use></svg>
+              <svg className="icon icon-logo c-footer__logos-item"><use xlinkHref="#icon-facebook"></use></svg>
+              <svg className="icon icon-logo c-footer__logos-item"><use xlinkHref="#icon-medium"></use></svg>
+              <svg className="icon icon-logo c-footer__logos-item"><use xlinkHref="#icon-instagram"></use></svg>
             </div>
             <span className="text -ff2-xs -white -center">Copyright © 2017 Radiant. All Rights Reserved.</span>
           </div>
