c4a5b8791b3690d9c56b7dcb9a45ea46e9caefe7
Hector Arce <hi@hectorarce.es>
2017-06-16 13:54:32 +0200

    improve slider react

--- src/layouts/Blog.js
+++ src/layouts/Blog.js
@@ -5,6 +5,66 @@ import Siema from 'siema';
 
 class Blog extends Component {
 
+  constructor () {
+    super();
+
+    this.blogContent = [
+      {
+        tag: 'NEW FEATURES',
+        title: 'Laying Tiles Without Grout and Mortar',
+        description:'On minimizing the latency of serving painted raster tiles.'
+      },
+      {
+        tag: 'NEWS',
+        title: 'Expanding Access to Earth Observation Data',
+        description:'Reflections on a SatSummit panel focused on how to expand access and distribution to earth imagery.'
+      },
+      {
+        tag: 'TUTORIALS',
+        title: 'Atomate analyses when updated imagery is available',
+        description:'Develop custom algorithms to extract information from your imagery and define automated pipelines to analyze your data.'
+      },
+      {
+        tag: 'TUTORIALS',
+        title: 'How to integrate the Radiant Earth API with your application',
+        description:'Learn how to create powerful insights and evidence-based support for change'
+      },
+      {
+        tag: 'NEW FEATURES',
+        title: 'Ingest your own data',
+        description:'Bring your own drone, manned-aerial, and satellite imagery into the Data Explorer with a few clicks.'
+      },
+      {
+        tag: 'NEWS',
+        title: 'Hello World',
+        description:'Today, I am really excited to introduce and announce Raster Foundry.'
+      }
+    ];
+
+    this.sliderContent = [
+      {
+        tag: 'NEW FEATURES',
+        title: 'Laying Tiles Without Grout and Mortar'
+      },
+      {
+        tag: 'NEWS',
+        title: 'Expanding Access to Earth Observation Data'
+      },
+      {
+        tag: 'TUTORIALS',
+        title: 'Atomate analyses when updated imagery is available'
+      },
+      {
+        tag: 'TUTORIALS',
+        title: 'How to integrate the Radiant Earth API with your application'
+      },
+      {
+        tag: 'NEW FEATURES',
+        title: 'Ingest your own data'
+      }
+    ];
+  }
+
   componentDidMount() {
     this.showSlider();
   }
@@ -16,39 +76,27 @@ class Blog extends Component {
       easing: 'ease-out',
       perPage: 1,
       startIndex: 0,
-      draggable: false,
+      draggable: true,
       threshold: 20,
       loop: false,
       onInit: () => {},
       onChange: () => {},
     });
 
-    Siema.prototype.addPagination = function() {
-      for (let i = 0; i < this.innerElements.length; i++) {
-        const btn = document.createElement('button');
-        btn.textContent = i;
-        btn.className = "c-slider__btn-slider"
-        if (i === 0) {
-          btn.classList.add('selected');
-        }
-        btn.addEventListener('click', () => this.goTo(i));
-        document.querySelector('.c-slider__container-btn').appendChild(btn);
-      }
+    const btnSlider = document.querySelectorAll('.c-slider__btn-slider');
+    btnSlider[0].classList.add('selected');
 
-      const btnSlider = document.querySelectorAll('.c-slider__btn-slider');
-
-      for (var i = 0; i < btnSlider.length; i++) {
-        btnSlider[i].addEventListener('click', function(event) {
-          for (var j = 0; j < btnSlider.length; j++) {
-            if (btnSlider[j].classList.contains('selected')) {
-              btnSlider[j].classList.remove('selected');
-            }
+    for (var i = 0; i < btnSlider.length; i++) {
+      btnSlider[i].addEventListener('click', function(event) {
+        for (var j = 0; j < btnSlider.length; j++) {
+          if (btnSlider[j].classList.contains('selected')) {
+            btnSlider[j].classList.remove('selected');
           }
-          this.classList.add('selected');
-        });
-      }
+        }
+        mySlider.goTo(parseInt(event.target.innerHTML));
+        this.classList.add('selected');
+      });
     }
-    mySlider.addPagination();
   }
 
   render() {
@@ -58,82 +106,35 @@ class Blog extends Component {
           <div className="l-blog">
             <div className="row">
               <div className="small-12 medium-9 large-7 small-offset-0 medium-offset-2 large-offset-5 columns l-blog__header c-slider" >
-                <div className="c-slider__container-btn" ref={(c) => { this.containerBtn = c; }}></div>
+                <div className="c-slider__container-btn columns" ref={(c) => { this.containerBtn = c; }}>
+                  {this.sliderContent.map((item, i) =>
+                    <button className="c-slider__btn-slider" key={i}>{i}</button>
+                  )}
+                </div>
                 <div ref={(c) => { this.slider = c; }}>
-                  <div className="c-slider__item_blog">
-                    <span className="text -ff1-m -color-2 -uppercase">Use Cases</span>
-                    <h1 className="text -ff2-xl -white -underline">How an Amazonas Moisture Index helped the Yanomami tribe</h1>
-                  </div>
-                  <div className="c-slider__item_blog">
-                    <span className="text -ff1-m -color-2 -uppercase">NEWS</span>
-                    <h1 className="text -ff2-xl -white -underline">Expanding Access to Earth Observation Data.</h1>
-                  </div>
-                  <div className="c-slider__item_blog">
-                    <span className="text -ff1-m -color-2 -uppercase">NEW FEATURES</span>
-                    <h1 className="text -ff2-xl -white -underline">On minimizing the latency of serving painted raster tiles.</h1>
-                  </div>
-                  <div className="c-slider__item_blog">
-                    <span className="text -ff1-m -color-2 -uppercase">NEWS</span>
-                    <h1 className="text -ff2-xl -white -underline">Expanding Access to Earth Observation Data.</h1>
-                  </div>
-                  <div className="c-slider__item_blog">
-                    <span className="text -ff1-m -color-2 -uppercase">Use Cases</span>
-                    <h1 className="text -ff2-xl -white -underline">How an Amazonas Moisture Index helped the Yanomami tribe</h1>
-                  </div>
+                  {this.sliderContent.map((item, i) =>
+                    <div key={i} className="small-12 columns l-blog__gallery-item">
+                      <div className="c-slider__item_blog">
+                        <span className="text -ff1-m -color-2 -uppercase">{item.tag}</span>
+                        <h1 className="text -ff2-xl -white -underline">{item.title}</h1>
+                      </div>
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
             <div className="row">
               <div className="small-12 columns l-blog__gallery">
                 <div className="row small-up-1 medium-up-3 large-up-3">
-
-                  <div className="small-3 columns l-blog__gallery-item">
-                    <div className="l-blog__gallery-item-picture"></div>
-                    <span className="text -ff1-xs -color-2 -uppercase">NEW FEATURES</span>
-                    <h2 className="text -ff2-l -white">Laying Tiles Without Grout and Mortar</h2>
-                    <p className="text -ff1-m -white">On minimizing the latency of serving painted raster tiles.</p>
-                  </div>
-
-                  <div className="small-3 columns l-blog__gallery-item">
-                    <div className="l-blog__gallery-item-picture"></div>
-                    <span className="text -ff1-xs -color-2 -uppercase">NEW FEATURES</span>
-                    <h2 className="text -ff2-l -white">Laying Tiles Without Grout and Mortar</h2>
-                    <p className="text -ff1-m -white">On minimizing the latency of serving painted raster tiles.</p>
-                  </div>
-
-                  <div className="small-3 columns l-blog__gallery-item">
-                    <div className="l-blog__gallery-item-picture"></div>
-                    <span className="text -ff1-xs -color-2 -uppercase">NEW FEATURES</span>
-                    <h2 className="text -ff2-l -white">Laying Tiles Without Grout and Mortar</h2>
-                    <p className="text -ff1-m -white">On minimizing the latency of serving painted raster tiles.</p>
-                  </div>
-
+                  {this.blogContent.map((item, i) =>
+                    <div key={i} className="small-3 columns l-blog__gallery-item">
+                      <div className="l-blog__gallery-item-picture"></div>
+                      <span className="text -ff1-xs -color-2 -uppercase">{item.tag}</span>
+                      <h2 className="text -ff2-l -white">{item.title}</h2>
+                      <p className="text -ff1-m -white">{item.description}</p>
+                    </div>
+                  )}
                 </div>
-
-                <div className="row small-up-1 medium-up-3 large-up-3 l-blog__gallery-second">
-
-                  <div className="small-3 columns l-blog__gallery-item">
-                    <div className="l-blog__gallery-item-picture"></div>
-                    <span className="text -ff1-xs -color-2 -uppercase">NEW FEATURES</span>
-                    <h2 className="text -ff2-l -white">Laying Tiles Without Grout and Mortar</h2>
-                    <p className="text -ff1-m -white">On minimizing the latency of serving painted raster tiles.</p>
-                  </div>
-
-                  <div className="small-3 columns l-blog__gallery-item">
-                    <div className="l-blog__gallery-item-picture"></div>
-                    <span className="text -ff1-xs -color-2 -uppercase">NEW FEATURES</span>
-                    <h2 className="text -ff2-l -white">Laying Tiles Without Grout and Mortar</h2>
-                    <p className="text -ff1-m -white">On minimizing the latency of serving painted raster tiles.</p>
-                  </div>
-
-                  <div className="small-3 columns l-blog__gallery-item">
-                    <div className="l-blog__gallery-item-picture"></div>
-                    <span className="text -ff1-xs -color-2 -uppercase">NEW FEATURES</span>
-                    <h2 className="text -ff2-l -white">Laying Tiles Without Grout and Mortar</h2>
-                    <p className="text -ff1-m -white">On minimizing the latency of serving painted raster tiles.</p>
-                  </div>
-
-              </div>
               </div>
             </div>
           </div>
