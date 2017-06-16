1c982f6f9f8330bf1857fd6f8c467037231ac00c
pjosh <jorgegar@gmail.com>
2017-06-16 10:37:01 +0200

    Home summary

--- src/layouts/Home.js
+++ src/layouts/Home.js
@@ -7,7 +7,18 @@ class Home extends Component {
     return (
       <div>
         <Header />
-        Hi!! This is the home
+        <div className="l-home">
+          <div className="l-home__block l-home__summary">
+            <div className="row align-center">
+              <div className="small-12 medium-8 columns">
+                <h1 className="text -ff2-xl -white"><span className="-underline">Earth Imagery for Impact</span></h1>
+              </div>
+              <div className="small-12 medium-8 columns">
+                <div className="l-home__description text -ff1-m -white">Radiant Earth provides a geospatial and imagery technology platform that supports knowledge transfer to positively impact the developing world’s greatest social, economic and environmental challenges.</div>
+              </div>
+            </div>
+          </div>
+        </div>
         <Footer />
       </div>
     )
