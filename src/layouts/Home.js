569354863b7473638766dc405166de4261e85ab7
pjosh <jorgegar@gmail.com>
2017-06-16 13:07:05 +0200

    Examples block

--- src/layouts/Home.js
+++ src/layouts/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import illustration1 from '../images/home/illustration-1@2X.png';
 
 class Home extends Component {
   render() {
@@ -15,6 +16,22 @@ class Home extends Component {
               </div>
               <div className="small-12 medium-8 columns">
                 <div className="l-home__description text -ff1-m -white">Radiant Earth provides a geospatial and imagery technology platform that supports knowledge transfer to positively impact the developing world’s greatest social, economic and environmental challenges.</div>
+              </div>
+            </div>
+          </div>
+          <div className="l-home__block l-home__example">
+            <div className="row">
+              <div className="small-12 medium-6 columns">
+                <div className="l-home__example-text">
+                  <div className="row">
+                    <div className="small-12 columns text -ff2-xs -color-2 -uppercase">Radiant earth api</div>
+                    <div className="small-12 columns l-home__example-text-2 text -ff2-l -white">Integrate geospatial data with your applications</div>
+                    <div className="small-12 columns l-home__example-text-3 text -ff1-m -white">Providing the global community with the greatest collection of satellite data can help create powerful insights and evidence-based support for change.</div>
+                  </div>
+                </div>
+              </div>
+              <div className="small-12 medium-6 columns l-home__example-img-container">
+                <img src={illustration1} />
               </div>
             </div>
           </div>
