25cfda386285b156ddaecd5bf58ea470974994e9
Hector <hi@hectorarce.es>
2017-08-27 12:49:15 +0200

    include functions to testimonial slider

--- client/src/layouts/Home.js
+++ client/src/layouts/Home.js
@@ -20,7 +20,28 @@ class Home extends Component {
     this.state = {
       slider: null,
       classCircleSelect: classCircle,
+      slideTestimonialNumber: 1,
+      slideTestimonialNext: true,
+      slideTestimonialBack: false,
     };
+  }
+
+  slideTestimonial(d) {
+    const e = this.sliderTestimonial;
+    const windowWidth = window.innerWidth;
+    const vwSlidePx = 30 / (windowWidth * 0.01);
+    let currentSlider;
+    currentSlider = d === 'next' ? currentSlider = this.state.slideTestimonialNumber + 1 : currentSlider = this.state.slideTestimonialNumber - 1;
+    e.style.transform = `translate3d(calc((((100vw / 2.7) * ${currentSlider - 1}) * (-1)) - ((${vwSlidePx}vw) * ${currentSlider - 1})), 0px, 0px)`;
+    this.setState({
+      slideTestimonialNumber: currentSlider,
+      slideTestimonialBack: currentSlider > 1,
+      slideTestimonialNext: currentSlider !== (document.getElementsByClassName('l-home__testimonial-item').length - 1),
+    });
+  }
+
+  slideCaseStudies() {
+
   }
 
   render() {
@@ -62,11 +83,29 @@ class Home extends Component {
 
           <div className="l-home__testimonial">
             <div className="row">
-              <span className="c-button-circle -right">
+              <span className={`c-button-circle -next -right ${this.state.slideTestimonialNext ? '-show' : ''}`} onClick={()=>this.slideTestimonial('next')}>
+                <svg className="icon icon-arrow-down"><use xlinkHref="#icon-arrow-down"></use></svg>
+              </span>
+
+              <span className={`c-button-circle -back -left ${this.state.slideTestimonialBack ? '-show' : ''}`} onClick={()=>this.slideTestimonial('back')}>
                 <svg className="icon icon-arrow-down"><use xlinkHref="#icon-arrow-down"></use></svg>
               </span>
               <h2 className="text -ff2-xs -color-2 columns -uppercase large-12 medium-12 small-12">TESTIMONIAL</h2>
-              <div className="l-home__testimonial-slider columns large-12 medium-12 small-12">
+              <div className="l-home__testimonial-slider columns large-12 medium-12 small-12" ref={(sliderTestimonial) => { this.sliderTestimonial = sliderTestimonial; }}>
+
+                <div className="l-home__testimonial-item">
+                  <div>
+                    <p className="text -ff2-m">
+                      <span className="quotes">“</span>
+                        This partnership fits us perfectly, as we can combine the operational nature of UNOSAT with the outreach and long-term impact Radiant.Earth is also looking for. Finally, we will have a platform and solutions in place for accessing the wealth of Earth observation data out there, and the ready-to-use products that will go along with them.
+                      <span className="quotes">”</span>
+                    </p>
+                  </div>
+                  <div className="author-contain">
+                    <span className="name text -color-1">- Einar Bjorgo, UNOSAT´s Manager</span>
+                    <div className="img"></div>
+                  </div>
+                </div>
 
                 <div className="l-home__testimonial-item">
                   <div>
